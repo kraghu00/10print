c7eb6a506195c670444ecf409db6930249243a79
Keshav Raghu <kraghu@uchicago.edu>
2022-02-09 18:11:07 -0600

    Framerate 46

--- 10print.js
+++ 10print.js
@@ -1,6 +1,13 @@
 // list of past 15 elections and percentage of electoral college votes that were democratic
-const lbj = 486/538; const nix = (538-301)/538; const nix2 = (538-520)/538; const car = 297/538; const rea = (538-489)/538; const rea2 = (538-525)/538; const hwb = (538-426)/538; const cli = 370/538; const cli2 = 379/538; const gwb = (538-271)/538; const gwb2 = (538-286)/538; const obm = 365/538; const obm2 = 332/538; const trmp = (538-304)/538; const bid = 306/538;
-const elecs = [lbj, nix, nix2, car, rea, rea2, hwb, cli, cli2, gwb, gwb2, obm, obm2, trmp, bid] // ekection data as array
+const lbj = 486/538; const nix = (538-301)/538; const nix2 = (538-520)/538;
+const car = 297/538; const rea = (538-489)/538; const rea2 = (538-525)/538;
+const hwb = (538-426)/538; const cli = 370/538; const cli2 = 379/538;
+const gwb = (538-271)/538; const gwb2 = (538-286)/538; const obm = 365/538;
+const obm2 = 332/538; const trmp = (538-304)/538; const bid = 306/538;
+const elecs = [
+   lbj, nix, nix2, car, rea, rea2, hwb, cli, cli2, gwb,
+   gwb2, obm, obm2, trmp, bid
+ ] // election data as array
 
 const w = process.stdout.columns
 
@@ -10,7 +17,7 @@ const w = process.stdout.columns
 // const BgBrightRed = '\x1b[91m'
 
 function draw () {
-  setTimeout(draw, 100)
+  setTimeout(draw, 46) // number of presidents in US history :)
   let output = ''
 
   for (let i = 0; i < w*2; i++) {
